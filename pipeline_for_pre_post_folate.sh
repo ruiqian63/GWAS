4b2ed4fc18534080f685f25a55329af415c18b5d
ruiqian63 <ruiqian0603@gmail.com>
2024-08-19 13:39:13 -0400

    Update pipeline_for_pre_post_folate.sh

--- pipeline_for_pre_post_folate.sh
+++ pipeline_for_pre_post_folate.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
 
+#split txt file
+awk '$3 == "TRUE"' folate_cohort.txt > post_folate.txt
+awk '$3 == "FALSE"' folate_cohort.txt > pre_folate.txt
+cut -f1 post_folate.txt > post_folate_iids.txt
+cut -f1 pre_folate.txt > pre_folate_iids.txt
+
+#filter the merged cohort by folate
+bcftools view -S post_folate_iids.txt -Oz -o post_folate.vcf.gz merged_all.vcf.gz
+bcftools view -S pre_folate_iids.txt -Oz -o pre_folate.vcf.gz merged_all.vcf.gz
