2715c16afe8f600e4b66f44a73668ca76f46dbce
ruiqian63 <ruiqian0603@gmail.com>
2024-08-19 14:13:35 -0400

    Update pipeline_for_pre_post_folate.sh

--- pipeline_for_pre_post_folate.sh
+++ pipeline_for_pre_post_folate.sh
@@ -9,3 +9,11 @@ cut -f1 pre_folate.txt > pre_folate_iids.txt
 #filter the merged cohort by folate
 bcftools view -S post_folate_iids.txt -Oz -o post_folate.vcf.gz merged_all.vcf.gz
 bcftools view -S pre_folate_iids.txt -Oz -o pre_folate.vcf.gz merged_all.vcf.gz
+
+#construct phenotype_list
+awk '{if($2 == "FALSE") print $1, 1; else if($2 == "TRUE") print $1, 2}' pre_folate.txt > phenotype_map.txt
+awk '{if($2 == "FALSE") print $1, 1; else if($2 == "TRUE") print $1, 2}' post_folate.txt > phenotype_map.txt
+
+#add phenotype
+awk 'NR==FNR {pheno[$1]=$2; next} {if ($1 in pheno) $6=pheno[$1]; print}' phenotype_map.txt pre_folate.fam > updated_phenotype.fam
+awk 'NR==FNR {pheno[$1]=$2; next} {if ($1 in pheno) $6=pheno[$1]; print}' phenotype_map.txt post_folate.fam > updated_phenotype.fam
