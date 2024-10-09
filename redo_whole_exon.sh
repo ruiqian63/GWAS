f4e08a479593f7b79f7d984c785ff9214e670444
ruiqian63 <ruiqian0603@gmail.com>
2024-10-09 17:47:03 -0400

    Update redo_whole_exon.sh

--- redo_whole_exon.sh
+++ redo_whole_exon.sh
@@ -5,3 +5,14 @@ sed '/^$/d' no_multi_case_no_chr_cleaned.vcf > no_multi_case_no_chr_cleaned_no_e
 #result file: no_multi_case_no_chr_cleaned_no_empty.vcf.gz
 
 #2. filter snps (remove indels) in both cohort
+bcftools view --types snps no_multi_case_no_chr_cleaned_no_empty.vcf.gz -Oz -o case_snps_only.vcf.gz
+bcftools view --types snps pass_sfari_sibs_hg19toHg38.vcf.gz -Oz -o control_snps_only.vcf.gz
+
+#3. find shared snps in both
+bcftools query -f '%CHROM\t%POS\t%ID\n' case_snps_only.vcf.gz | sort -u > case_snps.txt
+#number: 
+bcftools query -f '%CHROM\t%POS\t%ID\n' control_snps_only.vcf.gz | sort -u > control_snps.txt
+#number: 
+comm -12 case_snps.txt control_snps.txt > shared_snps.txt
+#number：
+
