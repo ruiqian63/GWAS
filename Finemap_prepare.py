229c84a2af69baf1ae857cbf5218f74fe0c60504
ruiqian63 <ruiqian0603@gmail.com>
2024-10-24 17:43:56 -0400

    Update Finemap_prepare.py

--- Finemap_prepare.py
+++ Finemap_prepare.py
@@ -4,6 +4,8 @@ import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 
+awk 'NR==1 || $11 == "ADD" {print $0}' post_gwas_logistic_results_all_pca10_plink2.PHENO1.glm.logistic.hybrid > post_filtered_results.glm.logistic.hybrid
+
 sumstats = gl.Sumstats("./pre_gwas_logistic_results_all_pca10_plink2.PHENO1.glm.logistic.hybrid",fmt="plink2")
 sumstats.basic_check()
 sumstats.get_lead()
@@ -19,14 +21,14 @@ locus.data.to_csv("sig_locus.tsv",sep="\t",index=None)
 locus.data["SNPID"].to_csv("sig_locus.snplist",sep="\t",index=None,header=None)
 
 plink \
-  --bfile "./pre_qc_updated_phenotype_all" \
+  --bfile "./post_qc_updated_phenotype_all" \
   --keep-allele-order \
   --r square \
   --extract sig_locus.snplist \
   --out sig_locus_mt
 
 plink \
-  --bfile "./pre_qc_updated_phenotype_all" \
+  --bfile "./post_qc_updated_phenotype_all" \
   --keep-allele-order \
   --r2 square \
   --extract sig_locus.snplist \
