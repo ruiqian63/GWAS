56d7566d291307f70b5bbe57b3571005b3ee496e
ruiqian63 <ruiqian0603@gmail.com>
2024-10-27 14:50:24 -0400

    Update and rename redo_folate.sh to redo_pre_post_folate.sh

--- redo_pre_post_folate.sh
+++ redo_pre_post_folate.sh
@@ -12,14 +12,14 @@
 ## commands
 source /programs/biogrids.shrc
 
-awk '$3 == "TRUE"' folate_cohort.txt > post_folate.txt
-awk '$3 == "FALSE"' folate_cohort.txt > pre_folate.txt
-cut -f1 post_folate.txt > post_folate_iids.txt
-cut -f1 pre_folate.txt > pre_folate_iids.txt
+#awk '$3 == "TRUE"' folate_cohort.txt > post_folate.txt
+#awk '$3 == "FALSE"' folate_cohort.txt > pre_folate.txt
+#cut -f1 post_folate.txt > post_folate_iids.txt
+#cut -f1 pre_folate.txt > pre_folate_iids.txt
 
 #filter the merged cohort by folate
-bcftools view -S post_folate_iids.txt -Oz -o post_folate.vcf.gz merged_1.vcf.gz
-bcftools view -S pre_folate_iids.txt -Oz -o pre_folate.vcf.gz merged_1.vcf.gz
+bcftools view -S post_folate_iids.txt -Oz -o post_folate.vcf.gz merged_folate.vcf.gz
+bcftools view -S pre_folate_iids.txt -Oz -o pre_folate.vcf.gz merged_folate.vcf.gz
 
 bcftools index pre_folate.vcf.gz
 bcftools index post_folate.vcf.gz
@@ -49,12 +49,19 @@ plink --bfile post_mind_filtered --geno 0.1 --make-bed --out post_geno_filtered
 plink --bfile post_geno_filtered --maf 0.05 --make-bed --out post_maf_filtered
 plink --bfile post_maf_filtered --hwe 0.05 --make-bed --out post_qc_updated_phenotype_all
 
-plink --bfile pre_qc_updated_phenotype_all --pca 10 --out pre_pca_results
-awk '{print $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12}' pre_pca_results.eigenvec > pre_pca_covar_10.txt
-plink --bfile post_qc_updated_phenotype_all --pca 10 --out post_pca_results
-awk '{print $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12}' post_pca_results.eigenvec > post_pca_covar_10.txt
+#plink --bfile pre_qc_updated_phenotype_all --pca 10 --out pre_pca_results
+#awk '{print $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12}' pre_pca_results.eigenvec > pre_pca_covar_10.txt
+#plink --bfile post_qc_updated_phenotype_all --pca 10 --out post_pca_results
+#awk '{print $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12}' post_pca_results.eigenvec > post_pca_covar_10.txt
 
-plink2 --bfile pre_qc_updated_phenotype_all --glm --covar pre_pca_covar_10.txt --covar-variance-standardize --allow-no-sex --out pre_gwas_logistic_results_all_pca10_plink2 
-plink2 --bfile pre_qc_updated_phenotype_all --glm --covar pre_pca_covar_10.txt --covar-variance-standardize --allow-no-sex --out pre_gwas_logistic_results_all_pca10_plink2 --adjust
-plink2 --bfile post_qc_updated_phenotype_all --glm --covar post_pca_covar_10.txt --covar-variance-standardize --allow-no-sex --out post_gwas_logistic_results_all_pca10_plink2
-plink2 --bfile post_qc_updated_phenotype_all --glm --covar post_pca_covar_10.txt --covar-variance-standardize --allow-no-sex --out post_gwas_logistic_results_all_pca10_plink2 --adjust
+plink --bfile pre_qc_updated_phenotype_all --pca 3 --out pre_pca_results
+awk '{print $1, $2, $3, $4, $5}' pre_pca_results.eigenvec > pre_pca_covar_3.txt
+plink --bfile post_qc_updated_phenotype_all --pca 3 --out post_pca_results
+awk '{print $1, $2, $3, $4, $5}' post_pca_results.eigenvec > post_pca_covar_3.txt
+
+
+
+#plink2 --bfile pre_qc_updated_phenotype_all --glm --covar pre_pca_covar_10.txt --covar-variance-standardize --allow-no-sex --out pre_gwas_logistic_results_all_pca10_plink2 
+#plink2 --bfile pre_qc_updated_phenotype_all --glm --covar pre_pca_covar_10.txt --covar-variance-standardize --allow-no-sex --out pre_gwas_logistic_results_all_pca10_plink2 --adjust
+#plink2 --bfile post_qc_updated_phenotype_all --glm --covar post_pca_covar_10.txt --covar-variance-standardize --allow-no-sex --out post_gwas_logistic_results_all_pca10_plink2
+#plink2 --bfile post_qc_updated_phenotype_all --glm --covar post_pca_covar_10.txt --covar-variance-standardize --allow-no-sex --out post_gwas_logistic_results_all_pca10_plink2 --adjust
