f93b48026ede59779787187a1cb2b852dcd10aa4
ruiqian63 <ruiqian0603@gmail.com>
2024-11-12 12:12:40 -0500

    Update run_EthSEQ_slurm.sh

--- run_EthSEQ_slurm.sh
+++ run_EthSEQ_slurm.sh
@@ -2,12 +2,12 @@
 
 #SBATCH --partition=bch-compute # queue to be used
 #SBATCH --time=48:00:00 # Running time (in hours-minutes-seconds)
-#SBATCH --job-name=EthSEQ_slurm_whole # Job name
-#SBATCH --output=EthSEQ_slurm_whole.%j.out # Name of the output file
-#SBATCH --error=EthSEQ_slurm_whole.%j.error
+#SBATCH --job-name=EthSEQ_slurm_folate # Job name
+#SBATCH --output=EthSEQ_slurm_folate.%j.out # Name of the output file
+#SBATCH --error=EthSEQ_slurm_folate.%j.error
 #SBATCH --nodes=1 # Number of compute nodes
 #SBATCH --ntasks=8 # Number of threads/tasks on one node
 #SBATCH --mem=512G
 
 export R_X=4.1
-Rscript stratify.R
+Rscript stratify_folate.R
