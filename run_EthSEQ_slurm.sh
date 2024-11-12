d47a9649a6337cd73c43d24188e2d46d32410505
ruiqian63 <ruiqian0603@gmail.com>
2024-11-12 11:42:33 -0500

    Create run_EthSEQ_slurm.sh

---
+++ run_EthSEQ_slurm.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#SBATCH --partition=bch-compute # queue to be used
+#SBATCH --time=48:00:00 # Running time (in hours-minutes-seconds)
+#SBATCH --job-name=EthSEQ_slurm_whole # Job name
+#SBATCH --output=EthSEQ_slurm_whole.%j.out # Name of the output file
+#SBATCH --error=EthSEQ_slurm_whole.%j.error
+#SBATCH --nodes=1 # Number of compute nodes
+#SBATCH --ntasks=8 # Number of threads/tasks on one node
+#SBATCH --mem=512G
+
+export R_X=4.1
+Rscript stratify.R
